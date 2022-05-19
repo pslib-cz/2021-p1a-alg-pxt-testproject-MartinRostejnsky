b60137988cb1c64687ce4418b2ca21311c159330
MartinRostejnsky <91248036+MartinRostejnsky@users.noreply.github.com>
2022-05-19 10:28:56 +0200

    jsem udelal srdicko mnau

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 basic.show_icon(IconNames.HEART)
+i = 0
 def on_forever():
-    pass
+    global i
+    i += 1
+    if i % 2 == 0: 
+        basic.show_icon(IconNames.HEART)
+    else:
+        basic.show_icon(IconNames.NO)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
 basic.showIcon(IconNames.Heart)
+let i = 0
 basic.forever(function on_forever() {
+    
+    i += 1
+    if (i % 2 == 0) {
+        basic.showIcon(IconNames.Heart)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
     
 })
